8a7b0a21f18f52f17b29a42fc04ed0bcc8f69f14
nazimamin <amin.nazim13@gmail.com>
2021-09-27 16:29:47 -0400

    adding husky support

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
     es2021: true,
     node: true
   },
-  extends: ['prettier'],
+  extends: ['eslint:recommended', 'prettier'],
   parserOptions: {
     ecmaVersion: 12,
     sourceType: 'module'
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+var port = 2000;
 
 app.get('/', (req, res) => res.send('Hello Husky!'));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
