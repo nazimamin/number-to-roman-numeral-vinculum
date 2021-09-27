4e91be88c2d17eed0a773dc6f1b7dc2d3b197cd5
nazimamin <amin.nazim13@gmail.com>
2021-09-27 16:58:21 -0400

    simplifyig .gtignore file to what is needed for this project

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-var port = 2000;
 
 app.get('/', (req, res) => res.send('Hello Husky!'));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
